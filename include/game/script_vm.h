558dcd7ca1c65662789401816b6b07620b879dd7
juliangrtz <julian.goeritz@protonmail.com>
2026-09-04 15:18:40 +0200

    Decompile script VM control flow

--- include/game/script_vm.h
+++ include/game/script_vm.h
@@ -12,6 +12,27 @@ enum ScriptVmCommandDescriptor {
     SCRIPT_VM_HAS_ARGUMENT_MODES = 0x40
 };
 
+enum ScriptVmResult {
+    SCRIPT_VM_CONTINUE = 0,
+    SCRIPT_VM_FINISHED = 1,
+    SCRIPT_VM_RETURNED = 2,
+    SCRIPT_VM_YIELDED = 3
+};
+
+enum ScriptVmJumpCondition {
+    SCRIPT_VM_EQUAL = 0,
+    SCRIPT_VM_NOT_EQUAL = 1,
+    SCRIPT_VM_LESS_THAN = 2,
+    SCRIPT_VM_GREATER_THAN = 3,
+    SCRIPT_VM_LESS_OR_EQUAL = 4,
+    SCRIPT_VM_GREATER_OR_EQUAL = 5,
+    SCRIPT_VM_BITS_SET = 6,
+    SCRIPT_VM_EITHER_NONZERO = 7,
+    SCRIPT_VM_VALUES_DIFFER = 8,
+    SCRIPT_VM_ZERO = 9,
+    SCRIPT_VM_NOT_NEGATIVE_ONE = 10
+};
+
 struct ScriptVm {
     u8 unknown_00[0x0C];
     const u32 *command_descriptors;
@@ -25,11 +46,25 @@ struct ScriptVmCommand {
     s32 arguments[16];
 };
 
+typedef struct ScriptVmState {
+    const u16 *script;
+    u8 unknown_04[0x60];
+    s32 stack[17];
+    u16 stack_depth;
+    u16 delay;
+} ScriptVmState;
+
 typedef char ScriptVmCommand_SizeCheck[
     sizeof(ScriptVmCommand) == 0x48 ? 1 : -1
 ];
+typedef char ScriptVmState_SizeCheck[sizeof(ScriptVmState) == 0xAC ? 1 : -1];
 
 s32 VM_ReadVariable(u16 variable, ScriptVm *vm, const u16 **script);
+int VM_CheckJumpCondition(int condition, s32 left, s32 right);
+int VM_ExecuteCommand(
+    ScriptVm *vm, ScriptVmState *state, ScriptVmCommand *command
+);
+int VM_Run(ScriptVm *vm, ScriptVmState *state);
 void VM_ReadCommand(ScriptVm *vm, const u16 **script, ScriptVmCommand *command);
 
 #endif
---
+++ src/game/script_vm_control.c
@@ -0,0 +1,48 @@
+#include <game/script_vm.h>
+
+int VM_CheckJumpCondition(int condition, s32 left, s32 right) {
+    switch (condition) {
+    case SCRIPT_VM_EQUAL:
+        return left == right;
+    case SCRIPT_VM_NOT_EQUAL:
+        return left != right;
+    case SCRIPT_VM_LESS_THAN:
+        return left < right;
+    case SCRIPT_VM_GREATER_THAN:
+        return left > right;
+    case SCRIPT_VM_LESS_OR_EQUAL:
+        return left <= right;
+    case SCRIPT_VM_GREATER_OR_EQUAL:
+        return left >= right;
+    case SCRIPT_VM_BITS_SET:
+        return (left & right) != 0;
+    case SCRIPT_VM_EITHER_NONZERO:
+        return (left | right) != 0;
+    case SCRIPT_VM_VALUES_DIFFER:
+        return (left ^ right) != 0;
+    case SCRIPT_VM_ZERO:
+        return left == 0;
+    case SCRIPT_VM_NOT_NEGATIVE_ONE:
+        return left != -1;
+    default:
+        return 0;
+    }
+}
+
+int VM_Run(ScriptVm *vm, ScriptVmState *state) {
+    ScriptVmCommand command;
+    int result;
+
+    if (state->delay != 0) {
+        state->delay--;
+        if (state->delay != 0) {
+            return SCRIPT_VM_YIELDED;
+        }
+    }
+
+    do {
+        VM_ReadCommand(vm, &state->script, &command);
+        result = VM_ExecuteCommand(vm, state, &command);
+    } while (result == SCRIPT_VM_CONTINUE);
+    return result;
+}
