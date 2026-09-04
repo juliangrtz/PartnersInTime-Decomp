7f3d7c304fce2f23fa40f74ec74e172614dabe6a
juliangrtz <julian.goeritz@protonmail.com>
2026-09-04 11:17:51 +0200

    Decompile battle status eligibility

--- include/game/battle_actor.h
+++ include/game/battle_actor.h
@@ -4,9 +4,10 @@
 #include <nitro.h>
 
 typedef struct BattleActor BattleActor;
+struct BattleSceneObject;
 
 struct BattleActor {
-    const void *resource;
+    struct BattleSceneObject *scene_object;
     s16 max_hp;
     s16 current_hp;
     s16 speed;
@@ -16,12 +17,14 @@ struct BattleActor {
     s16 hit_state;
     u8 unk_020[4];
     u16 flags;
+    u8 unk_026[0x46];
+    void *resource_slot;
 };
 
 int BattleActor_IsHpAtMostQuarter(BattleActor *actor);
 int BattleDamage_CalculateBase(int attacker_id, int defender_id, int scale_q8);
-struct BattleSceneObject;
 int BattleActor_ApplyDamage(struct BattleSceneObject *object, int damage);
+int BattleActor_CanReceiveStatus(BattleActor *actor);
 BattleActor *BattleActor_GetEnemySlot(int actor_id);
 BattleActor *BattleActor_GetPartySlot(int actor_id);
 BattleActor *BattleActor_GetById(int actor_id);
