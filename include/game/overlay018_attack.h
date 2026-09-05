41cf750fd7aeeee93ec406e9d2cac0433fa70af0
juliangrtz <julian.goeritz@protonmail.com>
2026-09-05 14:56:26 +0200

    Reconstruct overlay 18 attack object controller

--- include/game/overlay018_attack.h
+++ include/game/overlay018_attack.h
@@ -3,6 +3,10 @@
 
 #include <nitro.h>
 
+typedef struct Overlay18AttackObjectController
+    Overlay18AttackObjectController;
+typedef struct Overlay18AttackObjectSource Overlay18AttackObjectSource;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -17,6 +21,13 @@ int Overlay18Attack_GetTravelSpeedQ8(int progress);
 int Overlay18Attack_GetMotionScaleQ8(int progress);
 const s16 *Overlay18Attack_SelectThresholdRow(
     const s16 *table, int row_width, int value);
+void Overlay18Attack_ResetObjectController(
+    Overlay18AttackObjectController *controller);
+void Overlay18Attack_ClearObjectAnchor(
+    Overlay18AttackObjectController *controller);
+void Overlay18Attack_InitializeObjectController(
+    Overlay18AttackObjectController *controller, int variant,
+    Overlay18AttackObjectSource *source);
 
 #ifdef __cplusplus
 }
