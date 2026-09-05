e398b40d4be5d9b22975c2e3f852d801fbc6d25c
juliangrtz <julian.goeritz@protonmail.com>
2026-09-05 14:48:32 +0200

    Reconstruct overlay 16 variant object motion

--- include/game/overlay016_attack.h
+++ include/game/overlay016_attack.h
@@ -4,6 +4,7 @@
 #include <nitro.h>
 
 typedef struct Overlay16AttackObjectState Overlay16AttackObjectState;
+typedef struct Overlay16AttackTargetReference Overlay16AttackTargetReference;
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,6 +15,19 @@ void *Overlay16Attack_GetMotionRecord(u32 packed_index);
 int Overlay16Attack_IsObjectIdle(Overlay16AttackObjectState *state);
 int Overlay16Attack_IsObjectMotionComplete(
     Overlay16AttackObjectState *state);
+void Overlay16Attack_ResetObjectState(Overlay16AttackObjectState *state);
+void Overlay16Attack_InitializeObjectState(
+    Overlay16AttackObjectState *state, int variant);
+void Overlay16Attack_LaunchObjectAtReference(
+    Overlay16AttackObjectState *state,
+    Overlay16AttackTargetReference *reference);
+void Overlay16Attack_BeginObjectPhaseThree(
+    Overlay16AttackObjectState *state);
+void Overlay16Attack_BeginObjectPhaseTwo(
+    Overlay16AttackObjectState *state);
+void Overlay16Attack_BeginObjectTargetMotion(
+    Overlay16AttackObjectState *state,
+    Overlay16AttackTargetReference *reference, int duration);
 
 #ifdef __cplusplus
 }
--- src/overlay016/attack_state.h
+++ src/overlay016/attack_state.h
@@ -19,17 +19,51 @@ typedef struct Overlay16MotionPoint {
 
 typedef struct Overlay16AttackObjectState {
     BattleSceneObject *object;
-    void *effect;
     union {
-        u32 flags;
+        u32 motion_flags;
+        struct {
+            u32 motion_phase : 4;
+            u32 unknown_motion_04_31 : 28;
+        } motion_flag_bits;
+    };
+    union {
+        u32 control_flags;
+        u8 control_byte;
         struct {
             u8 state : 4;
             u8 variant : 2;
             u8 unknown_06_07 : 2;
-            u8 unknown_08_31[3];
-        } flag_bits;
+            u8 unknown_09;
+            u8 unknown_0a;
+            u8 unknown_0b;
+        } control_flag_bits;
     };
 } Overlay16AttackObjectState;
+
+typedef struct Overlay16AttackTargetReference {
+    BattleSceneObject *object;
+    u8 unknown_04[0x12];
+    union {
+        u8 flags;
+        struct {
+            u8 unknown_00_04 : 5;
+            u8 offset_variant : 2;
+            u8 unknown_07 : 1;
+        } flag_bits;
+    };
+} Overlay16AttackTargetReference;
+
+typedef struct Overlay16AttackTargetParameters {
+    u8 unknown_00[2];
+    s8 x;
+    s8 z;
+    u8 unknown_04;
+} Overlay16AttackTargetParameters;
+
+typedef struct Overlay16AttackTargetOffset {
+    u8 unknown_00[5];
+    Overlay16AttackTargetParameters parameters;
+} Overlay16AttackTargetOffset;
 
 typedef char Overlay16MotionPoint_SizeCheck[
     sizeof(Overlay16MotionPoint) == 8 ? 1 : -1];
@@ -41,5 +75,6 @@ extern Overlay16MotionPoint gOverlay16MotionTable[
 extern Overlay16MotionPoint gOverlay16MotionDeltas[
     OVERLAY16_MOTION_TABLE_COUNT];
 extern int gOverlay16MotionTableOffset;
+extern Overlay16AttackTargetOffset gOverlay16AttackTargetOffsets[];
 
 #endif
